e9b0094eb057f5cb8396dc5529c200125a94902a
AlexandreDps <90306622+AlexandreDps@users.noreply.github.com>
2023-05-05 05:40:23 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  5 05:30:29 2023
+
+@author: Alexandre
+"""
+
+from selenium.webdriver.chrome.options import Options
+from pymongo import MongoClient
+import threading
+import scrapping as s
+
+CHROME_OPTIONS = Options()
+CHROME_OPTIONS.add_argument('--headless')
+
+IDENTIFIANT = 'a_remplir'
+PASSWORD = 'a_remplir'
+IMPLICIT_WAIT = 5 #nb de secondes d'attente avant de retourner une erreur (le temps de charger les pages)
+
+#chromedriver112 ou chromedriver113 selon votre version de Chrome
+DRIVER_PATH = 'chromedriver112.exe'
+
+#Initialisation
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+db = client['StudentApp']
+
+
+def empty_collection(collection,db):
+    #Drop if exist (only for dev, a supprimer plus tard)
+    if collection in db.list_collection_names():
+        mycollection = db[collection]
+        mycollection.drop()
+    collection = db[collection]
+    collection.insert_one({})
+    
+empty_collection('homework',db)
+empty_collection('courses_marks',db)
+empty_collection('polypoints',db)
+
+
+t1 = threading.Thread(target=s.scrapping_polytech, 
+                      args=(IDENTIFIANT, PASSWORD, IMPLICIT_WAIT,CHROME_OPTIONS,DRIVER_PATH,db))
+t2 = threading.Thread(target=s.scrapping_moodle, 
+                      args=(IDENTIFIANT, PASSWORD, IMPLICIT_WAIT,CHROME_OPTIONS,DRIVER_PATH,db))
+
+t1.start()
+t2.start()
+t1.join()
+t2.join()                     
+                      
+print('Done')
+
--- scrapping.py
+++ scrapping.py
@@ -6,7 +6,77 @@ Created on Thu May  4 10:14:17 2023
 """
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-DRIVER_PATH = 'chromedriver.exe'
-driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get('https://intranet.univ-smb.fr/')
+######################### Scrapping moodle/intranet ##########################
+
+def scrapping_moodle(identifiant, password, implicit_wait,chrome_options,driver_path,db):
+    driver = webdriver.Chrome(executable_path=driver_path,options=chrome_options)
+    driver.get('https://intranet.univ-smb.fr/')
+    driver.implicitly_wait(implicit_wait)
+    
+    #Connexion
+    driver.find_element(By.ID, "username").send_keys(identifiant)
+    driver.find_element(By.ID, "password").send_keys(password)
+    driver.find_element(By.CLASS_NAME, "btn-submit").click()
+    driver.execute_script("window.open('https://moodle.univ-smb.fr/my/', '_blank');")
+    driver.switch_to.window(driver.window_handles[1]) #Switch sur l'onglet moodle
+    driver.find_element(By.PARTIAL_LINK_TEXT, 'Connexion').click()
+    driver.find_element(By.PARTIAL_LINK_TEXT, 'tudiant').click() #On met pas le É car l'ecriture pourrait changer dans le temps
+    
+    driver.get('https://moodle.univ-smb.fr/my/')
+    #Scrapping devoirs
+    devoirs = driver.find_elements(By.PARTIAL_LINK_TEXT, 'doit')
+    for i in range(len(devoirs)):
+        devoirs[i] = devoirs[i].get_attribute("aria-label")
+    collection = db['homework']
+    collection.insert_one({'user':identifiant,'devoirs':devoirs})
+    
+    #Scrapping notes et cours
+    driver.get("https://moodle.univ-smb.fr/grade/report/overview/index.php")
+    cours = driver.find_elements(By.CSS_SELECTOR, 'td.cell.c0')
+    notes = driver.find_elements(By.CSS_SELECTOR, 'td.cell.c1')
+    cours_notes = {'user':identifiant}
+    for i in range(len(cours)):
+        cours_notes[cours[i].text] = notes[i].text
+    #Scrapping planning
+    collection = db['courses_marks']
+    collection.insert_one(cours_notes)
+    
+    
+############################# Scrapping polytech #############################
+def scrapping_polytech(identifiant, password, implicit_wait,chrome_options,driver_path,db):
+    driver2 = webdriver.Chrome(executable_path=driver_path,options=chrome_options)
+    driver2.get('https://www.polytech.univ-smb.fr/intranet/accueil.html')
+    driver2.implicitly_wait(implicit_wait)
+    
+    #Connexion
+    driver2.find_element(By.CLASS_NAME, "tarteaucitronCTAButton.tarteaucitronDeny").click()
+    driver2.find_element(By.ID, "user").send_keys(identifiant)
+    driver2.find_element(By.ID, "pass").send_keys(password)
+    driver2.find_element(By.CLASS_NAME, "submit").click()
+    driver2.find_element(By.PARTIAL_LINK_TEXT, 'Espace').click()
+    
+    #Scrapping Polypoints
+    intitule = driver2.find_elements(By.CSS_SELECTOR, 'li.intitule')
+    intitule_tache = driver2.find_elements(By.CSS_SELECTOR, 'li.intitule_tache')
+    nb_points = driver2.find_elements(By.CSS_SELECTOR, 'li.nb_points')
+    annee = driver2.find_elements(By.CSS_SELECTOR, 'li.annee')
+    for i in range(len(intitule)):
+        intitule[i]= intitule[i].text
+        intitule_tache[i]= intitule_tache[i].text
+        nb_points[i]= nb_points[i].text
+        annee[i]= annee[i].text
+        #print(f'{intitule[i]}, {intitule_tache[i]}, {nb_points[i]}, {annee[i]}')
+    collection = db['polypoints']
+    collection.insert_one({'user':identifiant,
+                           'intitule':intitule,
+                           'intitule_tache':intitule_tache,
+                           'nb_points':nb_points,
+                           'annee':annee})
+    return [intitule,intitule_tache,nb_points,annee]
+
+
+
+
+
