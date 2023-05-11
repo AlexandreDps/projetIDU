7030a095ffca893b04af8ebdaf2bb8475d92864a
sanzianap <46610258+sanzianap@users.noreply.github.com>
2023-05-11 04:17:17 +0300

    Update login.php

--- login.php
+++ login.php
@@ -15,6 +15,7 @@ if (strcmp(strtoupper(trim($output)),"DONE") == 0)
 	#This command opens the file of the global variables that are stored in the $_SESSION array
 	session_start();
 	$_SESSION['username'] = $user;
+	$_SESSION['password'] = $psd;
 	#Calling the next php page in order to display the conversations
 	header("Location: cours.php");
 }
