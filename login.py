07b3f11987d4169dec37076ef52895855be746ba
AlexandreDps <90306622+AlexandreDps@users.noreply.github.com>
2023-05-11 02:12:48 +0200

    Update login.py

--- login.py
+++ login.py
@@ -39,17 +39,21 @@ def empty_collection(collection,db):
 empty_collection('homework',db)
 empty_collection('courses_marks',db)
 empty_collection('polypoints',db)
+empty_collection('marks_intranet',db)
 
 
 t1 = threading.Thread(target=s.scrapping_polytech, 
                       args=(IDENTIFIANT, PASSWORD, IMPLICIT_WAIT,CHROME_OPTIONS,DRIVER_PATH,db))
 t2 = threading.Thread(target=s.scrapping_moodle, 
                       args=(IDENTIFIANT, PASSWORD, IMPLICIT_WAIT,CHROME_OPTIONS,DRIVER_PATH,db))
-
+t3 = threading.Thread(target=s.scrapping_marks, 
+                      args=(IDENTIFIANT, PASSWORD, IMPLICIT_WAIT,CHROME_OPTIONS,DRIVER_PATH,db))
 t1.start()
 t2.start()
+t3.start()
 t1.join()
 t2.join()  
+t3.join() 
 
 db['polypoints'].delete_one({})
 db['homework'].delete_one({})
